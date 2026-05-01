c217792a0f454656bf3aebea1e8e7cc903f89758
rueeen <34107469+rueeen@users.noreply.github.com>
2026-04-30 23:53:06 -0400

    Remove PurchaseDigitalAccess references from digital_library

--- store_backend/digital_library/views.py
+++ store_backend/digital_library/views.py
@@ -1,12 +1,11 @@
 from rest_framework import generics, permissions
 
-from .models import PurchaseDigitalAccess
-from .serializers import PurchaseDigitalAccessSerializer
+from .serializers import DigitalLibraryItemSerializer
 
 
 class MyDigitalLibraryView(generics.ListAPIView):
     permission_classes = [permissions.IsAuthenticated]
-    serializer_class = PurchaseDigitalAccessSerializer
+    serializer_class = DigitalLibraryItemSerializer
 
     def get_queryset(self):
-        return PurchaseDigitalAccess.objects.filter(user=self.request.user).select_related("product")
+        return []
